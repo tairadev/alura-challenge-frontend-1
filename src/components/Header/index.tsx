38508fda6340a68905a7d99102429d34202c165e
Guilherme Henrique Taira <guilhermeht1906@gmail.com>
2025-02-02 23:29:54 -0300

    Sidebar finalizada

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,14 +5,18 @@ import Input from '../Input';
 
 const Header = () => {
   return (
-    <header className="container">
-      <img src={Logo} alt="AluraDev" className="logo" />
-      <Input
-        className="search-input"
-        name="search"
-        type="search"
-        placeholder="Busque por algo"
-      />
+    <header className='container'>
+      <div className='left-col'>
+        <div className='logo-container'>
+          <img src={Logo} alt='AluraDev' className='logo' />
+        </div>
+        <Input
+          className='search-input'
+          name='search'
+          type='search'
+          placeholder='Busque por algo'
+        />
+      </div>
       <HeaderIcons />
     </header>
   );
